11d78b5c2fc256895334978336c539bf3d694c51
earlbozarth <ehbbozarth@gmail.com>
2015-10-15 12:06:10 -0400

    Forum w/ Read Posts and Display Replies

--- src/Forum.java
+++ src/Forum.java
@@ -1,14 +1,46 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by earlbozarth on 10/15/15.
  */
 public class Forum {
     public static void main(String[] args) {
+        ArrayList<Post> posts = new ArrayList<>();
+        String postContent = readFile("posts.txt");
+        String [] lines = postContent.split("\n");
+
+        for(String line : lines){
+            String [] columns = line.split("\\|");
+            int replyID = Integer.valueOf(columns[0]);
+            String author = columns[1];
+            String text = columns[2];
+            Post post = new Post(replyID, author, text);
+            posts.add(post);
+        }//end of for loop
+
+        Scanner scanner = new Scanner(System.in);
+        int postID = -1;
+        while (true){
+            printPosts(posts, postID);
+            System.out.println("Enter in post ID to see replies");
+            postID = Integer.valueOf(scanner.nextLine());
+        }//end of while loop
 
     }//End of Main Method
+
+    static void printPosts(ArrayList<Post> posts, int postID){
+        int id = 0;
+        for (Post post : posts){
+            if(post.replyID == postID){
+                System.out.println(String.format("[%d] %s (%s)", id, post.text, post.author));
+            }//End of if statement
+            id++;
+        }//End of for loop
+    }//End of printPosts method
 
     static String readFile(String fileName){
         File f = new File(fileName);
---
+++ src/Post.java
@@ -0,0 +1,14 @@
+/**
+ * Created by earlbozarth on 10/15/15.
+ */
+public class Post {
+    int replyID;
+    String author;
+    String text;
+
+    public Post(int replyID, String author, String text) {
+        this.replyID = replyID;
+        this.author = author;
+        this.text = text;
+    }
+}
